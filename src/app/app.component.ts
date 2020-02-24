456f5c22384960a4993d2f5ab2782ca268aa3a34
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-02-24 17:31:10 -0600

    creamos el primer query para consultar users.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'loginRegisterJWT';
+
+  constructor(private apollo: Apollo){}
+
+  ngOnInit(): void {
+    const getUsers = gql`
+    query {
+      users {
+        id
+        name
+        lastname
+        email
+        registerDate
+      }
+    }
+    `;
+    this.apollo
+    .watchQuery({
+      query: getUsers,
+      fetchPolicy: 'network-only'
+    }).valueChanges.pipe(map((result: any) => {
+      return result.data.users;
+    })).subscribe((result) => {
+      console.log(result);
+    });
+  }
 }
