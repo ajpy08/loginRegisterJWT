2ec5f06c17ff2fe1feddab11d84648b14aae9191
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-02-25 10:14:18 -0600

    creamos la query de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,31 +8,63 @@ import { map } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'loginRegisterJWT';
 
-  constructor(private apollo: Apollo){}
+  constructor(private apollo: Apollo) {}
 
   ngOnInit(): void {
     const getUsers = gql`
-    query {
-      users {
-        id
-        name
-        lastname
-        email
-        registerDate
+      query {
+        users {
+          id
+          name
+          lastname
+          email
+          registerDate
+        }
       }
-    }
     `;
     this.apollo
-    .watchQuery({
-      query: getUsers,
-      fetchPolicy: 'network-only'
-    }).valueChanges.pipe(map((result: any) => {
-      return result.data.users;
-    })).subscribe((result) => {
-      console.log(result);
-    });
+      .watchQuery({
+        query: getUsers,
+        fetchPolicy: 'network-only'
+      })
+      .valueChanges.pipe(
+        map((result: any) => {
+          return result.data.users;
+        })
+      )
+      .subscribe(result => {
+        console.log(result);
+      });
+
+    const login = gql`
+      query login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+          status
+          message
+          token
+        }
+      }
+    `;
+
+    this.apollo
+      .watchQuery({
+        query: login,
+        variables: {
+          email: 'angelpuc08@gmail.com',
+          password: '123456'
+        },
+        fetchPolicy: 'network-only'
+      })
+      .valueChanges.pipe(
+        map((result: any) => {
+          return result.data.login;
+        })
+      )
+      .subscribe(result => {
+        console.log(result);
+      });
   }
 }
