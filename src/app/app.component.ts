fa4a5c543f249e928ac07ee92d006a4221ba76cf
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-02-25 10:25:43 -0600

    creamos query para me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
 import { map } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -61,6 +62,40 @@ export class AppComponent implements OnInit {
       .valueChanges.pipe(
         map((result: any) => {
           return result.data.login;
+        })
+      )
+      .subscribe(result => {
+        console.log(result);
+      });
+
+    const meData = gql`
+      query {
+        me {
+          status
+          message
+          user {
+            id
+            name
+            lastname
+            email
+          }
+        }
+      }
+    `;
+
+    this.apollo
+      .watchQuery({
+        query: meData,
+        fetchPolicy: 'network-only',
+        context: {
+          headers: new HttpHeaders({
+            authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjVlNTUyZmQxMzYzYjZjMjUwODY5ZTE5MyIsIm5hbWUiOiJKYXZpZXIiLCJsYXN0bmFtZSI6IlB1YyBZYW1hIiwiZW1haWwiOiJhbmdlbHB1YzA4QGdtYWlsLmNvbSIsImlkIjoxLCJyZWdpc3RlckRhdGUiOiIyMDIwLTAyLTI1IDA4OjMxOjQ1In0sImlhdCI6MTU4MjY0NzY0MywiZXhwIjoxNTgyNzM0MDQzfQ.SV1mdbHuRtpPdUDRjltTmiS-AA14jCmK65qv42AlcgA'
+          })
+        }
+      })
+      .valueChanges.pipe(
+        map((result: any) => {
+          return result.data.me;
         })
       )
       .subscribe(result => {
