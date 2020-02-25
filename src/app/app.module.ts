1f589503b9eb9c4bceb2af751204d0fd41a1482e
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-02-25 11:13:08 -0600

    creamos el componente login y modificamos app.component.html y agregamos el tag <app-login></app-login>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphqlModule } from './graphql/graphql.module';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
