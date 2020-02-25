486de57e352414f819fe74be5d01783d0a481aaa
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-02-25 11:06:47 -0600

    Modificamos el app.component para refactorizar los metodos en un servicio que se llamo api.service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,70 +1,31 @@
-import { Component, OnInit } from "@angular/core";
-import { Apollo } from "apollo-angular";
-import gql from "graphql-tag";
-import { map } from "rxjs/operators";
-import { HttpHeaders } from "@angular/common/http";
+import { ApiService } from './services/api.service';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { map } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
 import { getUsers, login, meData } from './operations/query';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = "loginRegisterJWT";
+  title = 'loginRegisterJWT';
 
-  constructor(private apollo: Apollo) {}
+  constructor(private apollo: Apollo, private api: ApiService) {}
 
   ngOnInit(): void {
-    this.apollo
-      .watchQuery({
-        query: getUsers,
-        fetchPolicy: "network-only"
-      })
-      .valueChanges.pipe(
-        map((result: any) => {
-          return result.data.users;
-        })
-      )
-      .subscribe(result => {
+    this.api.getUsers().subscribe(result => {
         console.log(result);
       });
 
-    this.apollo
-      .watchQuery({
-        query: login,
-        variables: {
-          email: "angelpuc08@gmail.com",
-          password: "123456"
-        },
-        fetchPolicy: "network-only"
-      })
-      .valueChanges.pipe(
-        map((result: any) => {
-          return result.data.login;
-        })
-      )
-      .subscribe(result => {
+    this.api.login('angelpuc08@gmail.com', '123456').subscribe(result => {
         console.log(result);
       });
 
-    this.apollo
-      .watchQuery({
-        query: meData,
-        fetchPolicy: "network-only",
-        context: {
-          headers: new HttpHeaders({
-            authorization:
-              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjVlNTUyZmQxMzYzYjZjMjUwODY5ZTE5MyIsIm5hbWUiOiJKYXZpZXIiLCJsYXN0bmFtZSI6IlB1YyBZYW1hIiwiZW1haWwiOiJhbmdlbHB1YzA4QGdtYWlsLmNvbSIsImlkIjoxLCJyZWdpc3RlckRhdGUiOiIyMDIwLTAyLTI1IDA4OjMxOjQ1In0sImlhdCI6MTU4MjY0NzY0MywiZXhwIjoxNTgyNzM0MDQzfQ.SV1mdbHuRtpPdUDRjltTmiS-AA14jCmK65qv42AlcgA"
-          })
-        }
-      })
-      .valueChanges.pipe(
-        map((result: any) => {
-          return result.data.me;
-        })
-      )
-      .subscribe(result => {
+    this.api.getMe('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjVlNTUyZmQxMzYzYjZjMjUwODY5ZTE5MyIsIm5hbWUiOiJKYXZpZXIiLCJsYXN0bmFtZSI6IlB1YyBZYW1hIiwiZW1haWwiOiJhbmdlbHB1YzA4QGdtYWlsLmNvbSIsImlkIjoxLCJyZWdpc3RlckRhdGUiOiIyMDIwLTAyLTI1IDA4OjMxOjQ1In0sImlhdCI6MTU4MjY0NzY0MywiZXhwIjoxNTgyNzM0MDQzfQ.SV1mdbHuRtpPdUDRjltTmiS-AA14jCmK65qv42AlcgA').subscribe(result => {
         console.log(result);
       });
   }
